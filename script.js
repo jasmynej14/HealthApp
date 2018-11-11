8053e3f5b31517c04ae44c6ecec22440bd37699a
jasmynej14 <44778592+jasmynej14@users.noreply.github.com>
2018-11-11 02:16:28 -0500

    abt

--- script.js
+++
@@ -1,8 +0,0 @@
-function valForm() {
-    var name, result;
-    name = document.getElementById("nameInput").value;
-    if (name.equals(" ")) {
-        result = "Please input name";
-    }
-    document.getElementById("result").innerHTML = result;
-}
